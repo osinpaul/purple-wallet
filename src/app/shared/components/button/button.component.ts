f18eaf4d2a8bbc996ec0e33cc4c7664ce2eb6d74
Pavel <spam@osinpaul.ru>
2025-04-15 11:53:19 +0300

    Создаём первый компонент. Кнопка для страницы авторизации

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ButtonComponent {}
