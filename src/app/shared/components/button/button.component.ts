d239289e808510d20b8ff27db49945e459cb70c7
Pavel <spam@osinpaul.ru>
2025-05-06 07:14:30 +0300

    Добавлено disabled состояние кнопки

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() title = '';
+  @Input() disabled = false;
 
   @Output() btnClick: EventEmitter<void> = new EventEmitter<void>();
 
