c4dcaef1a70529a46893b7be064c0acf7ff7bbd3
Pavel <spam@osinpaul.ru>
2025-05-06 07:00:52 +0300

    @Output события

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,4 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() title = '';
+
+  @Output() btnClick: EventEmitter<void> = new EventEmitter<void>();
+
+  onBtnClick(): void {
+    this.btnClick.emit();
+  }
 }
