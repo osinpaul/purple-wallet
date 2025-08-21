7c7744077bf92260d2500958af06af10f8c03c64
Pavel <spam@osinpaul.ru>
2025-08-21 09:35:59 +0300

    Layout страницы авторизации

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from './shared/components/button/button.component';
-import { InputComponent } from './shared/components/input/input.component';
-import { PasswordInputComponent } from './shared/components/password-input/password-input.component';
+import { PublicLayoutComponent } from './public/_layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent, InputComponent, PasswordInputComponent],
+  imports: [PublicLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'purple-wallet';
-
-  onInputChange(value: string): void {
-    console.log('Контрол изменился:', value);
-  }
-}
+export class AppComponent {}
