104cd8d9976d4a3fca550efe73a18fb5868c2a9d
Pavel <spam@osinpaul.ru>
2025-10-15 23:54:05 +0300

    Вычисляемые сигналы. Пример с computed

--- src/app/private/pages/exchange/exchange.component.ts
+++ src/app/private/pages/exchange/exchange.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
@@ -9,6 +9,7 @@ import { ButtonComponent } from '../../../shared/components/button/button.compon
 })
 export class ExchangeComponent {
   value = signal(0);
+  valueIn2 = computed(() => this.value() * 2);
 
   onUpdateClick(): void {
     this.value.update(v => v + 1);
