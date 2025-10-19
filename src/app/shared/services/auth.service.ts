4154dd2c4fb1fcca3810e243b227aec9d74720f5
Pavel <spam@osinpaul.ru>
2025-10-19 22:42:16 +0300

    Сервисы. Реализация сервиса авторизации

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
+import { delay, mergeMap, Observable, of, tap, throwError } from 'rxjs';
 
 @Injectable()
 export class AuthService {
   public isAuthenticated = true;
+
+  login$(username: string, password: string): Observable<boolean> {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isEmailValid = emailRegex.test(username);
+    const isPasswordValid = password.trim().length > 0;
+    const isAuthenticated = isEmailValid && isPasswordValid;
+
+    return of(isAuthenticated).pipe(
+      delay(100),
+      tap(v => (this.isAuthenticated = v)),
+      mergeMap(v =>
+        v ? of(v) : throwError(() => new Error('User login failed'))
+      )
+    );
+  }
+
+  logout$(): Observable<boolean> {
+    this.isAuthenticated = false;
+    return of(this.isAuthenticated).pipe(delay(100));
+  }
 }
