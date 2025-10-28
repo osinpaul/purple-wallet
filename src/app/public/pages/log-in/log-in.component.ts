aac7fca7c61ec9d608fa21bd5ed563b02658f75b
Pavel <spam@osinpaul.ru>
2025-10-28 23:27:07 +0300

    Template-Driven формы. Реализация ControlValueAccessor для контрола app-input

--- src/app/public/pages/log-in/log-in.component.ts
+++ src/app/public/pages/log-in/log-in.component.ts
@@ -5,10 +5,16 @@ import { InputComponent } from '../../../shared/components/input/input.component
 import { AuthService } from '../../../shared/services/auth.service';
 import { catchError, of, take, tap } from 'rxjs';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
-  imports: [ButtonComponent, InputComponent, PasswordInputComponent],
+  imports: [
+    ButtonComponent,
+    InputComponent,
+    PasswordInputComponent,
+    FormsModule,
+  ],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss',
   standalone: true,
@@ -16,7 +22,7 @@ import { Router } from '@angular/router';
 export class LogInComponent {
   private _authService: AuthService = inject(AuthService);
   private _router: Router = inject(Router);
-  private _formValue: { username: string | null; password: string | null } = {
+  public formValue: { username: string | null; password: string | null } = {
     username: null,
     password: null,
   };
@@ -25,14 +31,14 @@ export class LogInComponent {
 
   onInputChange(ctrl: 'username' | 'password', value: string): void {
     this.error = null;
-    this._formValue[ctrl] = value;
+    this.formValue[ctrl] = value;
   }
 
   onLoginClick(): void {
-    if (this._formValue.username == null || this._formValue.password == null)
+    if (this.formValue.username == null || this.formValue.password == null)
       return;
     this._authService
-      .login$(this._formValue.username, this._formValue.password)
+      .login$(this.formValue.username, this.formValue.password)
       .pipe(
         take(1),
         tap(() => this._router.navigate(['private'])),
--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -8,17 +12,47 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
   standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
+  private innerValue = '';
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onChange: (value: string) => void = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onTouched: () => void = () => {};
+
   @Input() iconUrl: string | null = null;
   @Input() type: 'text' | 'email' = 'text';
   @Input() placeholder = '';
-  @Input() disabled = false;
-  @Input() value = '';
-  @Output() controlValue: EventEmitter<string> = new EventEmitter<string>();
 
-  onInput(value: string) {
-    this.value = value;
-    this.controlValue.emit(value);
+  disabled = false;
+
+  writeValue(value: string): void {
+    this.innerValue = value ?? '';
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  handleInput(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    this.innerValue = value;
+    this.onChange(value);
   }
 }
