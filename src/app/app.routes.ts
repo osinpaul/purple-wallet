26cea7cbda2339924969f4599eb522155be9b138
Pavel <spam@osinpaul.ru>
2026-04-18 22:45:00 +0300

    Определение маршрутов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { PublicLayoutComponent } from './public/_layout/layout.component';
+import { PrivateLayoutComponent } from './private/_layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'public',
+    component: PublicLayoutComponent,
+  },
+  {
+    path: 'private',
+    component: PrivateLayoutComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'public',
+  },
+];
