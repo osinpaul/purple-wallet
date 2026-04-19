d955d42e27fa3d81b415e8aaaaba1619e7087b1e
Pavel <spam@osinpaul.ru>
2026-04-19 14:56:27 +0300

    Получение данных в Rates c использованием RxJs

--- src/app/private/pages/rates/rates.component.ts
+++ src/app/private/pages/rates/rates.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IRateModel } from './models/rate.model';
 import { FAKE_RATES } from '../../../shared/const/fake-rates.const';
 import { RateComponent } from './components/rate/rate.component';
+import { delay, Observable, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-rates',
   templateUrl: './rates.component.html',
   styleUrls: ['./rates.component.scss'],
   standalone: true,
-  imports: [RateComponent],
+  imports: [RateComponent, AsyncPipe],
 })
-export class RatesComponent {
-  rates: IRateModel[] = FAKE_RATES;
+export class RatesComponent implements OnInit {
+  rates$: Observable<IRateModel[]> = of(FAKE_RATES).pipe(delay(1000));
+
+  ngOnInit(): void {
+    this.rates$.subscribe(console.log);
+  }
 }
