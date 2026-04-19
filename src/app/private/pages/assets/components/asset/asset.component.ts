7d4a1072ca160dc4c38dbcab0ec238c5d2a9262b
Pavel <spam@osinpaul.ru>
2026-04-19 14:28:50 +0300

    Форматирование данных с использованием встроенных пайпов

--- src/app/private/pages/assets/components/asset/asset.component.ts
+++ src/app/private/pages/assets/components/asset/asset.component.ts
@@ -1,12 +1,26 @@
 import { Component, input } from '@angular/core';
 import { IAssetModel } from '../../models/asset.model';
-import { NgClass, NgOptimizedImage, UpperCasePipe } from '@angular/common';
+import {
+  CurrencyPipe,
+  DecimalPipe,
+  NgClass,
+  NgOptimizedImage,
+  PercentPipe,
+  UpperCasePipe,
+} from '@angular/common';
 
 @Component({
   selector: 'app-asset',
   templateUrl: './asset.component.html',
   styleUrls: ['./asset.component.scss'],
-  imports: [NgOptimizedImage, UpperCasePipe, NgClass],
+  imports: [
+    NgOptimizedImage,
+    UpperCasePipe,
+    NgClass,
+    CurrencyPipe,
+    PercentPipe,
+    DecimalPipe,
+  ],
 })
 export class AssetComponent {
   data = input.required<IAssetModel>();
