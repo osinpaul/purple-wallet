26da362737381e057fd3f017a45711135c6c7f42
Pavel <spam@osinpaul.ru>
2026-04-19 14:59:58 +0300

    Изменяемые сигналы. Пример с update

--- src/app/private/pages/exchange/exchange.component.ts
+++ src/app/private/pages/exchange/exchange.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-exchange',
-  standalone: true,
-  imports: [],
   templateUrl: './exchange.component.html',
-  styleUrl: './exchange.component.scss',
+  styleUrls: ['./exchange.component.scss'],
+  imports: [ButtonComponent],
 })
-export class ExchangeComponent {}
+export class ExchangeComponent {
+  value = signal(0);
+
+  onUpdateClick(): void {
+    this.value.update(v => v + 1);
+  }
+}
