5690941dc190e030391f9f4e759613afd029d448
Pavel <spam@osinpaul.ru>
2026-04-19 18:26:21 +0300

    Сервисы. Реализация сервиса авторизации

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { AuthService } from './shared/services/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    AuthService,
   ],
 };
--- src/app/public/pages/log-in/log-in.component.ts
+++ src/app/public/pages/log-in/log-in.component.ts
@@ -1,23 +1,46 @@
 import { ButtonComponent } from './../../../shared/components/button/button.component';
 import { PasswordInputComponent } from './../../../shared/components/password-input/password-input.component';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { InputComponent } from '../../../shared/components/input/input.component';
+import { AuthService } from '../../../shared/services/auth.service';
+import { catchError, of, take, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
-  imports: [
-    RouterLink,
-    ButtonComponent,
-    InputComponent,
-    PasswordInputComponent,
-  ],
+  imports: [ButtonComponent, InputComponent, PasswordInputComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss',
   standalone: true,
 })
 export class LogInComponent {
-  onInputChange(value: string): void {
-    console.log('Контрол изменился:', value);
+  private _authService: AuthService = inject(AuthService);
+  private _router: Router = inject(Router);
+  private _formValue: { username: string | null; password: string | null } = {
+    username: null,
+    password: null,
+  };
+
+  error: string | null = null;
+
+  onInputChange(ctrl: 'username' | 'password', value: string): void {
+    this.error = null;
+    this._formValue[ctrl] = value;
+  }
+
+  onLoginClick(): void {
+    if (this._formValue.username == null || this._formValue.password == null)
+      return;
+    this._authService
+      .login$(this._formValue.username, this._formValue.password)
+      .pipe(
+        take(1),
+        tap(() => this._router.navigate(['private'])),
+        catchError(err => {
+          this.error = err;
+          return of(err);
+        })
+      )
+      .subscribe();
   }
 }
